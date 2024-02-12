64bccc39d486f675afe12f5034192a23e0dbb191
viprko <pr.and.vi@gmail.com>
2024-02-12 17:27:32 +0200

    project structure changed

---
+++ authentication-service/src/test/java/pet/peranner/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package pet.peranner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthenticationServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
